f2257ac4a91b17f44fd33d09f2a26ccad418dc0a
Emanuel Kluge <mail@emanuel-kluge.de>
2021-06-12 15:54:03 +0200

    fix: instantiate URL correctly

--- assets/scripts/modules/tracking.js
+++ assets/scripts/modules/tracking.js
@@ -4,7 +4,7 @@ const TRACKER_ID =
   'ebc722246078b3a5588af466eb781b8944d306ef659e5ddb8d43383346e6f208_a064f4308cda5beebd28bec4ee89ef2e';
 
 export const tracking = () => {
-  const url = URL(TRACKER_URL);
+  const url = new URL(TRACKER_URL);
   const { pathname, search } = location;
 
   url.searchParams.append('id', TRACKER_ID);
