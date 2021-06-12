7ecd636eb6189aa6d355996ce9ef51127df1c3b8
Emanuel Kluge <mail@emanuel-kluge.de>
2021-06-12 15:51:46 +0200

    feat: add tracking

---
+++ assets/scripts/modules/tracking.js
@@ -0,0 +1,15 @@
+const TRACKER_URL = 'https://analytics.e5l.de/cctv.gif';
+
+const TRACKER_ID =
+  'ebc722246078b3a5588af466eb781b8944d306ef659e5ddb8d43383346e6f208_a064f4308cda5beebd28bec4ee89ef2e';
+
+export const tracking = () => {
+  const url = URL(TRACKER_URL);
+  const { pathname, search } = location;
+
+  url.searchParams.append('id', TRACKER_ID);
+  url.searchParams.append('resource', `${pathname}${search}`);
+  url.searchParams.append('referrer', document.referrer);
+
+  Object.assign(new Image(), { src: url.toString() });
+};
