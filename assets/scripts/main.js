dcd8639ef44bfb3c74bce40726fc288d4ad796c0
Emanuel Kluge <mail@emanuel-kluge.de>
2021-06-12 15:51:46 +0200

    feat: add tracking

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,10 @@
 import { archiveForwarder } from './modules/archive-fowarder';
+import { tracking } from './modules/tracking';
 
 const archiveForwarderHook = document.getElementById('archive-forwarder');
 
 if (archiveForwarderHook instanceof HTMLDivElement) {
   archiveForwarder();
 }
+
+tracking();
